434ea46ba51d1fc41b9a0206f421799b2a998823
Kalungi Moses <mdrewj@live.com>
2023-10-22 13:39:27 +0300

    Removed incompatible Package
    Uninstalled nuxt-content-assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,6 @@ export default defineNuxtConfig({
     "@nuxtjs/plausible",
     // https://github.com/nuxt/devtools
     "@nuxt/devtools",
-    "nuxt-content-assets",
     "@nuxthq/studio",
   ],
   runtimeConfig: {
